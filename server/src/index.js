cee93703b2363540878dbc7a88235214bb1d119b
Will Rutland <wrutland07@gmail.com>
2025-06-07 17:36:11 +0100

    switched to a js backend

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/api/hello', (_req:any, res:any) => {
-  res.json({ message: 'Hello from server!' });
+app.get('/', (_req, res) => {
+  res.send("Hello from '/'");
 });
 
 app.listen(PORT, () => {
