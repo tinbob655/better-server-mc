bcce67f7c73020b6c041241ac24b592b3b768eb4
Will Rutland <wrutland07@gmail.com>
2025-12-23 19:28:13 +0000

    fixed footer formatting

--- client/src/components/multiPageComponents/footer.tsx
+++ client/src/components/multiPageComponents/footer.tsx
@@ -5,11 +5,13 @@ export default function Footer():React.ReactElement {
 
     return (
         <React.Fragment>
-            <div className="dividerLine" id="footer"></div>
-            <img src="/logo.png" className="logo" style={{height: '400px'}} />
-            <p>
-                Website created by <a href="https://github.com/tinbob655" style={{textDecoration: 'underline'}}>Tinbob655</a> for the Better Server community.
-            </p>
+            <div id="footer">
+                <div className="dividerLine"></div>
+                <img src="/logo.png" className="logo" style={{height: '400px'}} />
+                <p>
+                    Website created by <a href="https://github.com/tinbob655" style={{textDecoration: 'underline'}}>Tinbob655</a> for the Better Server community.
+                </p>
+            </div>
         </React.Fragment>
     );
 };
