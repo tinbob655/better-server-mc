acdbb9d5f0056b2ca896b036a8cd3796bbfdf533
Will Rutland <wrutland07@gmail.com>
2025-12-25 10:50:17 +0000

    made backend use pm2

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const cors = require('cors');
-
-//get routes
+const app = express();
 const serverStatus = require('./routes/serverStatus');
 
-
 app.use(cors());
-app.listen(8080, () => {
-    console.log('Server listening on port 8080');
-});
-app.get('/', (req, res) => {
-    res.send("Hello from backend!");
-});
 
+// Serve static files from the React build
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 //routes
 app.use('/serverStatus', serverStatus);
+
+// Fallback: serve index.html for React Router
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
