8569ae71412ca95fdbe5248a90b32b8d239411d5
Will Rutland <wrutland07@gmail.com>
2025-12-27 17:17:32 +0000

    iframe test #2

--- client/src/components/pages/map/mapPage.tsx
+++ client/src/components/pages/map/mapPage.tsx
@@ -5,7 +5,7 @@ export default function MapPage():React.ReactElement {
 
     return (
         <React.Fragment>
-            <iframe src="http://localhost:8100" allowFullScreen />
+            <iframe src="http://better-server.world:8100" allowFullScreen />
         </React.Fragment>
     );
 };
