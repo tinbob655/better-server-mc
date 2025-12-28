5ea0321acd624d0ceffd7e03b2ca156cc0796cbb
Will Rutland <wrutland07@gmail.com>
2025-12-28 11:46:30 +0000

    added world map

--- client/src/components/pages/map/mapPage.tsx
+++ client/src/components/pages/map/mapPage.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import PageHeader from '../../multiPageComponents/pageHeader';
+import fullscreenButtonImage from '../../../assets/images/fullscreenButton.svg';
 
 
 export default function MapPage():React.ReactElement {
+
+    const worldMapIframe = useRef<HTMLIFrameElement>(null);
+    const fullscreenButton = useRef<HTMLButtonElement>(null);
+
+    function toggleFullscreen(): void {
+        worldMapIframe.current?.requestFullscreen();
+    }
+
+    function mapHovered():void {
+        fullscreenButton.current?.classList.add('shown');
+    };
+
+    function mapUnhovered():void {
+        fullscreenButton.current?.classList.remove('shown');
+    };
 
     return (
         <React.Fragment>
             <PageHeader title="World Map" subtitle="Explore a live world map" />
 
-            {/* actual map. NOTE: will only render on the server */}
-            <iframe
-                src="https://better-server.world/bluemap/" title="Bluemap" id="worldMapIframe" allowFullScreen />
+            <div id="worldMapWrapper" onMouseOver={mapHovered} onMouseLeave={mapUnhovered}>
+                <button onClick={toggleFullscreen} type="button" id="worldMapFullscreenButton" ref={fullscreenButton}>
+                    <img src={fullscreenButtonImage} className="button" />
+                </button>
+                
+                {/* actual map */}
+                <iframe src="https://better-server.world/bluemap/" title="Bluemap" id="worldMapIframe" allowFullScreen ref={worldMapIframe} />
+            </div>
+
         </React.Fragment>
     );
 };
