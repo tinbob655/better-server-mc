a3703d01a3f1385647c8b5e46d1e15a903415c1c
tinbob655 <wrutland07@gmail.com>
2026-08-30 21:41:53 +0100

    wrote basic auth system for backend

--- server/src/main/java/world/betterserver/server/service/userDetails/UserDetailsService.java
+++ server/src/main/java/world/betterserver/server/service/userDetails/UserDetailsService.java
@@ -5,10 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import world.betterserver.server.model.entity.user.User;
 import world.betterserver.server.model.entity.user.UserRepository;
 
 @RequiredArgsConstructor
+@Service
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
 
     private final UserRepository userRepository;
