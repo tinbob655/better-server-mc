fa0e9c465649fb7fabbd238a5c210bdba929da97
tinbob655 <wrutland07@gmail.com>
2026-08-30 20:43:48 +0100

    setup all pages

--- client/src/AllRoutes.tsx
+++ client/src/AllRoutes.tsx
@@ -3,9 +3,23 @@ import {Route, Routes} from 'react-router';
 
 //import all pages
 const Home = lazy(() => import('./pages/home/Home.tsx'));
+const Account = lazy(() => import('./pages/account/Account.tsx'));
+const Map = lazy(() => import('./pages/map/Map.tsx'));
+const News = lazy(() => import('./pages/news/News.tsx'));
+const Polls = lazy(() => import('./pages/polls/Polls.tsx'));
+const ServerStatus = lazy(() => import('./pages/serverStatus/ServerStatus.tsx'));
+const Suggestions = lazy(() => import('./pages/suggestions/Suggestions.tsx'));
+const Wiki = lazy(() => import('./pages/wiki/Wiki.tsx'));
 
 const pageInfo: [string, React.ComponentType][] = [
     ['', Home],
+    ['account', Account],
+    ['map', Map],
+    ['news', News],
+    ['polls', Polls],
+    ['serverStatus', ServerStatus],
+    ['suggestions', Suggestions],
+    ['wiki', Wiki],
 ];
 
 export default function AllRoutes(): React.ReactElement {
