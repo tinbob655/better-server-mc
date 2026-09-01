31dbbd1366a9671bf3c2db651aa7397ef51fd738
tinbob655 <wrutland07@gmail.com>
2026-09-01 20:28:15 +0100

    created footer

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './footer.scss';
 
 export default function Footer():React.ReactElement {
 
     return (
         <footer>
+            <img src={"/logo.png"} alt={"The Better Server logo"} />
+            <p>
+                Website created by <a href={"https://tinbob655.github.io/tinbob655/"} target={"_blank"}>Tinbob655</a>
+                {' '}for the Better Server community.
+                <br/>
+                Want your own website? Find my socials <a href={"https://tinbob655.github.io/tinbob655/"} target={"_blank"}>here.</a>
+            </p>
         </footer>
     )
 }
