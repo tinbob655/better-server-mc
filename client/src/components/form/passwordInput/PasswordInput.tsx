5739fd0bb87d74f4ce44121b0a1a8ed6ff540eef
tinbob655 <wrutland07@gmail.com>
2026-09-02 16:54:49 +0100

    created password input component

---
+++ client/src/components/form/passwordInput/PasswordInput.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import FormGroup from "../FormGroup.tsx";
+import './passwordInput.scss';
+import eyeOpen from '../../../assets/images/buttons/eyeOpen.svg';
+import eyeClosed from '../../../assets/images/buttons/eyeClosed.svg';
+
+interface PasswordInputParams {
+    confirm?: boolean;
+    value: string;
+    setValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function PasswordInput({confirm, value, setValue}: PasswordInputParams): React.ReactElement {
+
+    const [passwordShown, setPasswordShown] = useState<boolean>(false);
+
+    return (
+        <div className={"passwordInputWrapper"}>
+            <FormGroup
+                name={confirm ? "confirmPassword" : "password"}
+                value={value}
+                setValue={setValue}
+                label={confirm ? "Confirm password" : "Password"}
+                type={passwordShown ? "text" : "password"}
+            />
+
+            <button
+                type={"button"}
+                onClick={() => setPasswordShown(prev => !prev)}
+                aria-label={passwordShown ? "Hide password" : "Show password"}
+                aria-pressed={passwordShown}
+            >
+                <img src={passwordShown ? eyeClosed : eyeOpen} alt={`${passwordShown ? "Hide" : "Show"} password`} />
+            </button>
+        </div>
+    )
+}
