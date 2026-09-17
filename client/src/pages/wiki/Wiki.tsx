acb4905a6dc608aeb1daeff264ef997f32f0cdf2
tinbob655 <wrutland07@gmail.com>
2026-09-17 17:05:53 +0100

    added search to wiki

--- client/src/pages/wiki/Wiki.tsx
+++ client/src/pages/wiki/Wiki.tsx
@@ -3,6 +3,9 @@ import PageHeader from "../../components/PageHeader.tsx";
 import GenericMarkupSection from "../../components/genericMarkupSection/GenericMarkupSection.tsx";
 import useWiki from "../../hooks/useWiki.ts";
 import FancyButton from "../../components/fancyButton/FancyButton.tsx";
+import SearchBar from "../../components/searchBar/SearchBar.tsx";
+import useSearch from "../../hooks/useSearch.ts";
+import type {WikiSummary} from "../../types/wiki";
 
 const  SingleWikiPost = lazy(() => import("./singleWikiPost/SingleWikiPost.tsx"));
 const NewWikiPostForm = lazy(() => import("./NewWikiPostForm.tsx"));
@@ -18,6 +21,8 @@ export default function Wiki(): React.ReactElement {
         deleteWikiPost
     } = useWiki();
 
+    const {search, setSearch, filteredItems} = useSearch<WikiSummary>(summaries, s => [s.title])
+
     const [postingNew, setPostingNew] = useState<boolean>(false);
 
     return (
@@ -30,8 +35,15 @@ export default function Wiki(): React.ReactElement {
                     Here is a list of wiki posts our community has made! To read more on a post, just click on it. Further
                     down the page you will be able to make posts of your own.
                 </p>
+                <SearchBar
+                    placeholder={"Search the wiki..."}
+                    value={search}
+                    onChange={setSearch}
+                    alignment={"RIGHT"}
+                />
                 {fetchError && <p className={"errorText"}>{fetchError}</p>}
-                {summaries.map(summary => (
+                <div className={"sectionDivider light"} style={{marginBottom: '0.5rem', maxWidth: '55%'}} />
+                {filteredItems.map(summary => (
                     <Suspense>
                         <SingleWikiPost
                             key={summary.title}
