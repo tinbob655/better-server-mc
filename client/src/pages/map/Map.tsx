ba33dbf4164373ed2574974b8ed37acd932c386a
tinbob655 <wrutland07@gmail.com>
2026-09-20 14:01:36 +0100

    hide map for unauthenticated users

--- client/src/pages/map/Map.tsx
+++ client/src/pages/map/Map.tsx
@@ -1,9 +1,15 @@
-import React, {useRef} from 'react';
+import React, {useRef, lazy, Suspense} from 'react';
 import PageHeader from "../../components/PageHeader.tsx";
 import './map.scss'
 import IconButton from "../../components/iconButton/IconButton.tsx";
+import {useAuth} from "../../context/auth/AuthContext.tsx";
+
+const GenericMarkupSection = lazy(() => import("../../components/genericMarkupSection/GenericMarkupSection.tsx"));
+const FancyButton = lazy(() => import("../../components/fancyButton/FancyButton.tsx"));
 
 export default function Map(): React.ReactElement {
+
+    const {user} = useAuth();
 
     const wrapperRef = useRef<HTMLDivElement>(null);
     const iframeRef = useRef<HTMLIFrameElement>(null);
@@ -11,10 +17,11 @@ export default function Map(): React.ReactElement {
     function toggleFullscreen(): void {
         if (document.fullscreenElement) {
             document.exitFullscreen()
-                .catch(err => console.error("Failed to exit fullscreen:", err));
-        } else {
+                .catch(err => console.error("Failed to exit fullscreen: ", err));
+        }
+        else {
             iframeRef.current?.requestFullscreen()
-                .catch(err => console.error("Failed to enter fullscreen:", err));
+                .catch(err => console.error("Failed to enter fullscreen: ", err));
         }
     }
 
@@ -22,21 +29,33 @@ export default function Map(): React.ReactElement {
         <React.Fragment>
             <PageHeader title={"Map"} subtitle={"A live map of the server!"} />
 
-            <div id={"bluemapWrapper"} ref={wrapperRef}>
-                <IconButton
-                    imageLoader={() => import("../../assets/images/buttons/fullscreen.svg")}
-                    showElementRef={wrapperRef}
-                    onClick={toggleFullscreen}
-                    alt={"Toggle fullscreen"}
-                />
-                <iframe
-                    id={"bluemapFrame"}
-                    src={"https://better-server.world/bluemap"}
-                    title={"Server Bluemap"}
-                    allow={"fullscreen"}
-                    ref={iframeRef}
-                />
-            </div>
+            {user ? (
+                <div id={"bluemapWrapper"} ref={wrapperRef}>
+                    <IconButton
+                        imageLoader={() => import("../../assets/images/buttons/fullscreen.svg")}
+                        showElementRef={wrapperRef}
+                        onClick={toggleFullscreen}
+                        alt={"Toggle fullscreen"}
+                    />
+                    <iframe
+                        id={"bluemapFrame"}
+                        src={"https://better-server.world/bluemap"}
+                        title={"Server Bluemap"}
+                        allow={"fullscreen"}
+                        ref={iframeRef}
+                    />
+                </div>
+            ) : (
+                <Suspense>
+                    <GenericMarkupSection title={"Access denied"}>
+                        <p className={"errorText"}>
+                            You don't have permission to see the map
+                        </p>
+                        <FancyButton label={"Log in / sign up here!"} to={"/account"} />
+                    </GenericMarkupSection>
+                </Suspense>
+            )}
+
         </React.Fragment>
     )
 }
